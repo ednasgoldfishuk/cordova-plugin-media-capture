a1a9b62615b4d4b378ced195640b2d44332d576c
Phil Lidstone <plidstone@gmail.com>
2013-11-12 12:06:47 +0000

    added overlay view to plugin.xml

---
+++ src/ios/OverlayView.h
@@ -0,0 +1,16 @@
+//
+//  OverlayView.h
+//  SprkleProducer
+//
+//  Created by Philip Lidstone on 11/09/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverlayView : UIView
+
+- (void)scanButtonTouchUpInside;
+- (void)clearLabel:(UILabel *)label;
+
+@end
